be9c91c42d817f746d88dc49373631b1a016689a
AlexPetr0vsky <dr.alexpetrovsky@yandex.ru>
2023-12-29 08:55:25 -0800

    Changed pages/base_page.py, pages/login_page.py, pages/main_page.py and test_main_page.py

--- pages/login_page.py
+++ pages/login_page.py
@@ -9,7 +9,8 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        assert "login" in self.browser.current_url, "Url should contains 'login'"
+        current_url = self.browser.current_url
+        assert "login" in current_url, f"URL '{current_url}' should contains 'login'"
 
     def should_be_login_form(self):
         assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is absent"
