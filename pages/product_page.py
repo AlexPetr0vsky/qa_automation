7c1eb5d566e6bd6244e5ea53c94f51c7ff9dfcdb
AlexPetr0vsky <dr.alexpetrovsky@yandex.ru>
2024-01-04 07:19:24 -0800

    Minor changes of pages/product_page.py and test_main_page.py

--- pages/product_page.py
+++ pages/product_page.py
@@ -1,6 +1,5 @@
 from .base_page import BasePage
 from .locators import *
-import time
 
 
 class ProductPage(BasePage):
@@ -15,7 +14,6 @@ class ProductPage(BasePage):
     def add_to_basket(self):
         add_to_basket_btn = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BTN)
         add_to_basket_btn.click()
-        # self.solve_quiz_and_get_code()
 
     def should_be_the_same_book_name(self, book_name):
         book_name_in_basket = self.browser.find_element(*BasketLocators.BOOK_IN_BASKET).text
